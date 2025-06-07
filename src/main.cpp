4900589590b9c022362aee28ddc1b802100cdcdd
FedeLupianez <lupianezfederico@gmail.com>
2025-06-07 16:31:56 -0300

    Agrego la librería ArduinoJson para manejar el json de las response y para convertir string a json para pasarlo como body en las querys

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,36 @@
-#include <Arduino.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
 #include "Urls.h"
+#include <Arduino.h>
+#include <ArduinoJson.h> // Librería para manejar json de una mejor manera
+#include <HTTPClient.h>
+#include <WiFi.h>
 
-const char* ssid = "INTERNET";
-const char* password = "PASSWORD";
+const char *ssid = "INTERNET";
+const char *password = "PASSWORD";
 
-String requestGet(HTTPClient &client, String Url){
+// Objeto para trabajar con json, asigna s tamaño dinámicamente
+JsonDocument jsonDoc;
+
+String requestGet(HTTPClient &client, String Url) {
   // función para realizar una request
   client.begin(Url);
   int responseCode = client.GET();
 
-  if (responseCode <= 0){
+  if (responseCode <= 0) {
     return ("Error");
   }
-
+  // Transformo la respuesta a String
   String response = client.getString();
+
   client.end(); // Elimino los recursos utilizados
   return (response);
 }
-
 
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
 
   Serial.println("Esperando conexión ... ");
-  while (WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     delay(100);
     Serial.println("...");
   }
@@ -34,11 +38,11 @@ void setup() {
 
   HTTPClient http; // Defino un objeto http
   // Defino la url a la que apunta
-  // En este caso hago que apunte al endpoint para obtener 
+  // En este caso hago que apunte al endpoint para obtener
   // el id del generador
   String response = requestGet(http, GetIdUrl);
+  deserializeJson(jsonDoc, response.c_str());
   Serial.println(response);
 }
 
-void loop() {
-}
+void loop() {}
