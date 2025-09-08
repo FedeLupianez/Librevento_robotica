239b95e5040c38bb9f6ee5af02ad7aaffa436e7d
FedeLupianez <lupianezfederico@gmail.com>
2025-09-08 17:07:06 -0300

    Agrego un manejo de estados y chequeo de la hora para medir o no

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,75 @@
 
-#include "HTTPClient.h"
-#include "WiFi.h"
 #include "Generador/Generador.h"
-#include <Arduino.h>
-#include "WiFiUdp.h"
+#include "HTTPClient.h"
 #include "NTPClient.h"
+#include "WiFi.h"
+#include "WiFiUdp.h"
 #include <Adafruit_INA219.h>
+#include <Arduino.h>
 
-const char *email_usuario = "johndoe@gmail.com";
-const char *ssid = "Manuel de Falla";
-const char *password = "Fallaalumno";
-const int voltage_input_pin = 36;
+const char* email_usuario = "johndoe@gmail.com";
+const char* ssid = "Manuel de Falla";
+const char* password = "Fallaalumno";
 
 Adafruit_INA219 ina219;
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", 0, 60000);
+NTPClient timeClient(ntpUDP, "pool.ntp.org", -10800, 60000);
 
-const char* meditions_hours[] = {"00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"};
-const int meditions_cant = 24;
+void setup()
+{
+    Serial.begin(9600);
+    WiFi.begin(ssid, password);
+    HTTPClient http;
 
-// bool is_time_to_measure(int current_hour, int current_minute){
-//   for (int i = 0; i < meditions_cant; i++){
-//     int med_hour = atoi(meditions_hours[i].substr(0, 2).c_str());
-//     int med_minute = atoi(meditions_hours[i].substr(3, 5).c_str());
-//     if (current_hour == med_hour && current_minute == med_minute){
-//       return true;
-//     }
-//   }
-//   return false;
-// }
-
-void setup() {
-  Serial.begin(9600);
-  WiFi.begin(ssid, password);
-  HTTPClient http;
-
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println("\nWifi conectado");
-  timeClient.begin();
-  if (!ina219.begin()){
-    Serial.println("Error al iniciar INA219");
-    while (1){ delay(10); }
-  }
-  Serial.println("INA219 iniciado");
-  ina219.setCalibration_32V_1A();
-  //setup_macaddress(http);
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println("\nWifi conectado");
+    timeClient.begin();
+    if (!ina219.begin()) {
+        Serial.println("Error al iniciar INA219");
+        while (true) {
+            delay(10);
+        }
+    }
+    Serial.println("INA219 iniciado");
+    ina219.setCalibration_32V_1A();
+    // setup_macaddress(http);
 }
 
-void loop() {
-  float voltaje = ina219.getBusVoltage_V();
-  Serial.println("Voltaje: " + String(voltaje));
+enum States {
+    normal,
+    reading,
+};
+
+States actual_state = normal;
+int last_minute = -1;
+
+States get_new_state(States previus_state)
+{
+    float current_minute = timeClient.getMinutes();
+    if (current_minute == 0
+        && current_minute != last_minute) {
+
+        last_minute = current_minute;
+        return States::reading;
+    }
+
+    last_minute = current_minute;
+    return States::normal;
+}
+
+void loop()
+{
+    timeClient.update();
+    actual_state = get_new_state(actual_state);
+    if (actual_state != States::normal) {
+        if (actual_state == States::reading) {
+            // Tomar la medición cada hora
+            float voltaje = ina219.getBusVoltage_V();
+            Serial.println("Voltaje: " + String(voltaje));
+        }
+    }
 }
